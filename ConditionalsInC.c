ad49b600280e1296bd4264392a435013c542ed00
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-15 14:57:31 -0700

    compare, conditionals example in C

--- ConditionalsInC.c
+++ ConditionalsInC.c
@@ -1,10 +1,18 @@
 
 #include <stdio.h>
 
-
-
 int main(void) {
 
+    int x = 2;
+    int y = 1;
+
+    if (x < y) {
+
+    } else if (x > y) {
+
+    } else if (x == y) {
+        
+    }
     
 }
 
