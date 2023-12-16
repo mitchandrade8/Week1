2ee7374171c48f632f96bb0197cb14d5365d632e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-15 17:54:39 -0700

    refactored the meow Example with a while loop

--- meow.c
+++ meow.c
@@ -4,8 +4,21 @@
 int main(void) 
 {
     // would be better to add a for loop
-    
+
+    // First Version:
+    /*
     printf("meow\n");
     printf("meow\n");
     printf("meow\n");
+    */
+
+   // Refactored Version with For loop:
+
+   int counter = 3;
+   while (counter > 0)
+   {
+    printf("meow\n");
+    counter = counter - 1;
+   }
+   
 }
