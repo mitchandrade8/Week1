f3a2925cf18165260ff6728cbafb13f7b921d51a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-16 13:29:47 -0700

    --Continuing Week 1 Lecture with examples

--- meow.c
+++ meow.c
@@ -3,7 +3,7 @@
 
 int main(void) 
 {
-    // would be better to add a for loop
+    // would be better to add a loop
 
     // First Version:
     /*
@@ -12,7 +12,7 @@ int main(void)
     printf("meow\n");
     */
 
-   // Refactored Version with For loop:
+   // Refactored Version with While loop:
 
    int counter = 3;
    while (counter > 0)
@@ -20,5 +20,19 @@ int main(void)
     printf("meow\n");
     counter = counter - 1;
    }
-   
+
+   int i = 3;
+   while (i > 0) 
+   {
+    printf("moew\n");
+    i--;
+   }
+
+   int i = 1;
+   while (i <= 3)
+   {
+    printf("meow\n");
+    i++;
+   }
+
 }
