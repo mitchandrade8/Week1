434b77d531850bdc81c48c920edaa5d95a35359c
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-21 13:31:28 -0700

    calculator week 0 example

--- Notes.c
+++ Notes.c
@@ -7,3 +7,9 @@
 // Break an Infinite Loop
     /// Control C
 
+
+// Pseudocode:
+    /// is terse English that gets your point across, like for the phone
+    /// searching like last time.
+
+// Use comments to breakdown the problem and help get started
--- mario.c
+++ mario.c
@@ -5,6 +5,7 @@ int main(void)
 {
     const int n = 5;
 
+    // Printing a grid of bricks:
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
