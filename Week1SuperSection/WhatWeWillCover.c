ab319705cb1fb4d1ed2aa06bf2a52a2f80341a98
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-27 05:06:57 -0700

    Start of SuperSection Week 1

---
+++ Week1SuperSection/WhatWeWillCover.c
@@ -0,0 +1,10 @@
+
+// Why are we uaing C?
+
+// How can we read and write code that inlcudes variables, conditionals, and loops?
+
+// Why do we care about data types?
+
+// What does it mean to compile a C program?
+
+// How many years will it take to double our Llamas? 
