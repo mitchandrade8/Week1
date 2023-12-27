051aa2698e49f57e38428a8cee1e47102447aab4
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-27 05:17:45 -0700

    SuperSection Exercise

---
+++ Week1SuperSection/variablesEx.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    int calls = 4;
+    calls = 5;
+
+    int x = 50;
+
+    printf("calls is %i\n", calls);
+}
+
+// Why does C care about data types??
+
+/// 8-bit UTF Code 
+
+// Format Codes:
+/// int (%i)     long (%li)    char (%c)
+/// float (%f)   double (%f)   string (%s)
